c77d338d94eb512a5f645b492bff69a9469ff7c6
perfr <per.fredelius@gmail.com>
2021-10-13 21:42:26 +0200

    editor view now rendering

--- src/components/StrategyWindow/MapEditor.tsx
+++ src/components/StrategyWindow/MapEditor.tsx
@@ -2,10 +2,16 @@ import React from "react";
 import {TopEditorBar} from "./GUIComponents/TopEditorBar";
 import {SceneWrapper} from "./Scene/SceneView";
 import {EditorSimulator} from "./MapEditor/EditorSimulator";
+import {Edges} from "./GameComponents/Edges";
+import {Nodes} from "./GameComponents/Nodes";
+import {SceneContext} from "./SceneState/SceneContext";
 
 function EditorScene() {
-  return <SceneWrapper>
-
+  return <SceneWrapper
+    bridgeContexts={[SceneContext]}
+  >
+    <Edges />
+    <Nodes />
   </SceneWrapper>;
 }
 
